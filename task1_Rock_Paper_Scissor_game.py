2d3407ef86610d18274753a0d8a70c1c72f94441
mdshahjan0206-sketch <mdshahjan0206@gmail.com>
2026-06-20 21:56:46 +0530

    Refactor : Rock Paper Scissors game logic

--- task1_Rock_Paper_Scissor_game.py
+++ task1_Rock_Paper_Scissor_game.py
@@ -1,48 +1,24 @@
 import random
 
-user_score = 0
-computer_score = 0
+choices = ["rock", "paper", "scissors"]
 
-while True:
-    print("\n--- Rock Paper Scissors Game ---")
-    print("1. Rock")
-    print("2. Paper")
-    print("3. Scissors")
+user = input("Enter rock, paper, or scissors: ").lower()
 
-    user = input("Enter your choice: ").lower()
+computer = random.choice(choices)
 
-    choices = ["rock", "paper", "scissors"]
-    computer = random.choice(choices)
+print("You chose:", user)
+print("Computer chose:", computer)
 
-    print("Computer chose:", computer)
+if user not in choices:
+    print("Invalid choice!")
 
-    if user == computer:
-        print("It's a Tie!")
+elif user == computer:
+    print("It's a tie!")
 
-    elif (user == "rock" and computer == "scissors") or \
-         (user == "paper" and computer == "rock") or \
-         (user == "scissors" and computer == "paper"):
-        print("You Win!")
-        user_score += 1
+elif (user == "rock" and computer == "scissors") or \
+     (user == "paper" and computer == "rock") or \
+     (user == "scissors" and computer == "paper"):
+    print("You win!")
 
-    elif user in choices:
-        print("Computer Wins!")
-        computer_score += 1
-
-    else:
-        print("Invalid Choice!")
-        continue
-
-    print("\nScore")
-    print("You:", user_score)
-    print("Computer:", computer_score)
-
-    play_again = input("\nPlay Again? (yes/no): ").lower()
-
-    if play_again != "yes":
-        break
-
-print("\nFinal Score")
-print("You:", user_score)
-print("Computer:", computer_score)
-print("Thanks for playing!")
+else:
+    print("Computer wins!")
